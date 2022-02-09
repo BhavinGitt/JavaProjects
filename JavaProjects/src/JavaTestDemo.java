cc22c08c32999e635e8a94fce18fe83a7e7e8d67
BhavinGitt <bhavinkumarram@gmail.com>
2022-02-08 20:13:21 -0500

    Update on test

--- JavaProjects/src/JavaTestDemo.java
+++ JavaProjects/src/JavaTestDemo.java
@@ -4,7 +4,7 @@ public class JavaTestDemo {
 
 	public static void main(String[] args) {
 		for (int j=1;j<=5;j++) {
-			for (int i=1;i<=10;i++) {
+			for (int i=1;i<10;i++) {
 				if (i%2==0) 
 					System.out.print("  "+i+"  ");
 			}
