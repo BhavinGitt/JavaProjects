c31769d7fd81795df9cba239fa72120309dd79fa
BhavinGitt <bhavinkumarram@gmail.com>
2022-02-25 01:18:22 -0500

    update on sorting techniques

--- JavaProjects/src/homeworkJAVA/ShapeClass.java
+++ JavaProjects/src/homeworkJAVA/ShapeClass.java
@@ -5,51 +5,65 @@ import java.util.Comparator;
 
 public class ShapeClass {
 
-public static void main(String[] args) {
-		
+	public static void main(String[] args) {
+
 		Shape circle = new Shape("CIRCLE", 4); // 3.14 * 16 = 50.24
 		Shape square = new Shape("SQUARE", 10); // 100
 		Shape rec = new Shape("REC", 4, 10); // 40
-		
-		Shape[] shapes = {circle, square, rec};
-		
-				
+
+		Shape shapes[] = { circle, square, rec };
+
+		Shape temp1 = null;
+		System.out.println(" Array Sort -----two techniques");
+		for (int i = 0; i < shapes.length; i++) {
+			Shape s1 = shapes[i];
+			for (int j = i + 1; j < shapes.length; j++) {
+				Shape s2 = shapes[j];
+				if (s1.getArea() > s2.getArea()) {
+					temp1 = s1;
+					shapes[i] = s2;
+					shapes[j] = temp1;
+				}
+			}
+		}
+
+		System.out.print("Array after Sorting is..\n");
+		for (Shape y : shapes) {
+			System.out.print(y.getArea() + "    ");
+		}
+
 		double largestArea = 0;
 		Shape temp = null;
-		
-		
-		for(int i=0; i<shapes.length; i++) {
+
+		for (int i = 0; i < shapes.length; i++) {
 			Shape s = shapes[i];
-				if(largestArea < s.getArea()) {
+			if (largestArea < s.getArea()) {
 				largestArea = s.getArea();
 				temp = s;
-				
+
 			}
-			
-			System.out.println("-------------");
+
+			// System.out.println("-------------");
 			System.out.println("Shape Name:" + s.getName());
 			System.out.println("Shape Area:" + s.getArea());
 			System.out.println("-------------");
 		}
-		
-		
+
 		System.out.println("Largest area Name:" + temp.getName());
 		System.out.println("-------------");
 		System.out.println("-------------");
-		System.out.println("Before sorting:"+Arrays.toString(shapes));
+		System.out.println("Before sorting:" + Arrays.toString(shapes));
 		Arrays.sort(shapes);
-		System.out.println("Ascending order After sorting:"+Arrays.toString(shapes));
+		System.out.println("Ascending order After sorting:" + Arrays.toString(shapes));
 		Arrays.sort(shapes, Comparator.reverseOrder());
-		System.out.println("Descending order After sorting:"+Arrays.toString(shapes));
-		
-		
+		System.out.println("Descending order After sorting:" + Arrays.toString(shapes));
+
 	}
 
+}
 
-}	
+class Shape implements Comparable<Shape> {
 
-class Shape  implements Comparable <Shape> {
-	
 	private String name;
 	private int length;
 	private int width;
@@ -64,20 +78,20 @@ class Shape  implements Comparable <Shape> {
 		this.length = length;
 		setArea();
 	}
-	
+
 	public Shape(String name, int length, int width) {
 		this.name = name;
 		this.length = length;
 		this.width = width;
 		setArea();
 	}
-	
+
 	private void setArea() {
-		if(name.equals("CIRCLE")) {
+		if (name.equals("CIRCLE")) {
 			this.area = PI * length * length;
-		} else if(name.equals("SQUARE")) {
+		} else if (name.equals("SQUARE")) {
 			this.area = length * length;
-		} else if(name.equals("REC")) {
+		} else if (name.equals("REC")) {
 			this.area = length * width;
 		} else {
 			this.area = 0;
@@ -112,8 +126,6 @@ class Shape  implements Comparable <Shape> {
 		return area;
 	}
 
-	
-
 	@Override
 	public String toString() {
 		return "Shape [name=" + name + ", length=" + length + ", width=" + width + ", area=" + area + "]";
@@ -121,11 +133,8 @@ class Shape  implements Comparable <Shape> {
 
 	@Override
 	public int compareTo(Shape o) {
-		
+
 		return (int) (this.area - o.area);
 	}
 
-	
-
-	
 }
