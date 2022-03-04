681a72926a8c22b62455d7e3776c7dad308acc36
BhavinGitt <97195233+BhavinGitt@users.noreply.github.com>
2022-03-04 11:42:17 -0500

    Update SumTest.java

--- JavaProjects/src/SumTest.java
+++ JavaProjects/src/SumTest.java
@@ -6,6 +6,7 @@ public class SumTest {
 	public static void main(String[] args) {
 		int sum1=0;
         System.out.prit(sum1);
+		 System.out.prit("created for the test "+sum1);
 		for (int i=1;i<=10;i++) 
 		{
 			sum1=sum1+i;	
