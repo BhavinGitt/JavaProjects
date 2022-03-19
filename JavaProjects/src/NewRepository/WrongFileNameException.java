7b6063302a254a94895d4475f755a3021d38de9d
BhavinGitt <bhavinkumarram@gmail.com>
2022-03-18 20:03:53 -0400

    Usage of TreeSet, CustomeException and SeleniumTest

---
+++ JavaProjects/src/NewRepository/WrongFileNameException.java
@@ -0,0 +1,27 @@
+package NewRepository;
+
+public class WrongFileNameException extends Exception {
+	
+
+	public WrongFileNameException(String errorMessage) {
+		super(errorMessage);
+	}
+	
+//	public static void method() throws WrongFileNameException {
+//	    throw new WrongFileNameException("This is CheckedException");  
+//	}
+
+	public static void main(String[] args) throws WrongFileNameException {
+	//	method();
+		
+		try {
+			throw new WrongFileNameException("this is the Custome-EXCEPTION");
+			
+		} catch (WrongFileNameException ABC) {
+			System.out.println(ABC.getMessage());
+			  
+			
+		}
+
+	}
+}
